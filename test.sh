d70a11ce087a01931acab3dab23be47507bb5b38
DennisPeriquet <DennisPeriquet@users.noreply.github.com>
2024-01-16 15:19:16 -0500

    Automated - test of prcreator by dperique
    Testing of prcreator by dperique

---
+++ test.sh
@@ -0,0 +1,8 @@
+~/prcreator \
+        -github-token-path=/tmp/oauth \
+        -organization=dperique \
+        -repo=asus-rt-n66r-openvpn \
+        -git-message="Testing of prcreator by dperique" \
+        -pr-message "This is a prcreator test by dperique" \
+        -pr-title="Automated - test of prcreator by dperique" \
+		-pr-assignees="DennisPeriquet,neisw,dgoodwin"
